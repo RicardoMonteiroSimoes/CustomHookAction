eed9ffb2f1481d65e6a5f32a1b42aa1dccf6936f
Ricardo Simoes <ricardo_simoes_93@hotmail.com>
2020-01-27 21:10:47 +0100

    first draft of the action done

---
+++ index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const repourl = core.getInput('repourl');
+    const reponame = core.getInput('reponame');
+    const targetip = core.getInput('targetip');
+    const targetport = core.getInput('targetport');
+    const message = core.getInput('message');
+
+    console.log("Setting up the following payload:");
+    if (message) {
+        const body = {
+            reponame: reponame,
+            repourl: repourl,
+            message: message,
+        }
+    } else {
+        const body = {
+            reponame: reponame,
+            repourl: repourl,
+        }
+    }
+
+    console.log(targetip + ":" + targetport);
+    console.log(JSON.stringify(body));
+
+    fetch(targetip + ":" + targetport, {
+        method: "POST",
+        body: JSON.stringify(body)
+    });
+
+    console.log("Done! :)")
+
+} catch (error) {
+    core.setFailed(error.message);
+}
